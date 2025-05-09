66cae15bda9c979694c5d31920d68daf14310bd5
dageitnere <darja.geitnere@gmail.com>
2025-05-09 03:23:59 +0300

    акт
    актттт

--- exit_conf.php
+++
@@ -1,5 +0,0 @@
-<?php
-    setcookie('user', '', time() - 3600, "/"); // Удаляем cookie
-    header('Location: index.php'); // Перенаправление на главную страницу
-    exit();
-?>
--- exit_worker_conf.php
+++
@@ -1,5 +0,0 @@
-<?php
-    setcookie('worker', '', time() - 3600, "/"); // Удаляем cookie
-    header('Location: workeraunth.html'); // Перенаправление на workeraunth.html
-    exit();
-?>
